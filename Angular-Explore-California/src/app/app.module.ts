5f65065febe4631c6e45a451c94ea1731b300f6c
Derrick Kyle Ward <dward330@gmail.com>
2018-06-06 21:29:21 -0400

    To Sync Up
    To Sync Up

--- Angular-Explore-California/src/app/app.module.ts
+++ Angular-Explore-California/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PageBottomComponent } from './page-bottom/page-bottom.component';
@@ -13,6 +14,7 @@ import { PageHeaderComponent } from './page-header/page-header.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([{path: '', component: AppComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
